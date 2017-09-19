0b62eba412e4a4204190f73c57146fc57710b1e3
drkohlipk <drkohlipk@gmail.com>
2017-09-18 17:53:06 -0700

    Complete with Python subsets

--- 18_sept_17/subsets.py
+++ 18_sept_17/subsets.py
@@ -3,8 +3,7 @@ def make_subsets(sofar, arr, length):
         print sofar
         return
 
-    for i in range(length):
-        make_subsets(sofar + [arr], arr[:-1], length-1)
-        make_subsets(sofar + [arr[:i] + arr[i+1:]], arr[:i] + arr[i+1:-1], length-1-i)
+    make_subsets(sofar + [arr[0]], arr[:0] + arr[1:], length-1)
+    make_subsets(sofar, arr[:0] + arr[1:], length-1)
 
 make_subsets([], [1, 2, 3], 3)
