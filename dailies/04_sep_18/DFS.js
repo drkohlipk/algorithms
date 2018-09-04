c390be143049da9cbbeb7bb7ed8be0da9f8a7796
drkohlipk <drkohlipk@gmail.com>
2018-09-04 17:33:37 -0600

    Dailies

---
+++ dailies/04_sep_18/DFS.js
@@ -0,0 +1,22 @@
+const GraphSearchTester = require('../../helpers/testing/graphs/GraphSearchTester');
+
+const depthFirstSearch = (root, id, visited = new Set()) => {
+	if (!root) return undefined;
+	else if (root.id === id) return root;
+
+	visited.add(root);
+
+	let n = undefined;
+
+	for (const child of root.children) {
+		if (!visited.has(child)) {
+			n = depthFirstSearch(child, id, visited);
+		}
+
+		if (n) break;
+	}
+
+	return n;
+};
+
+new GraphSearchTester(depthFirstSearch);
