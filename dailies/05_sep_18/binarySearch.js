2555b7137d547e5cd43eae2589efb0e7495324ea
drkohlipk <drkohlipk@gmail.com>
2018-09-05 13:37:19 -0600

    Daily dailies

---
+++ dailies/05_sep_18/binarySearch.js
@@ -0,0 +1,18 @@
+const ArraySearchTester = require('../../helpers/testing/arrays/ArraySearchTester');
+
+const binarySearch = (arr, x) => {
+	let begIdx = 0;
+	let endIdx = arr.length - 1;
+
+	while (begIdx <= endIdx) {
+		const mid = Math.floor((endIdx - begIdx) / 2) + begIdx;
+
+		if (arr[mid] === x) return true;
+		else if (arr[mid] <= x) begIdx = mid + 1;
+		else endIdx = mid - 1;
+	}
+
+	return false;
+};
+
+new ArraySearchTester(binarySearch);
