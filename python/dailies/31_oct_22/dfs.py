49edf4377b9f6e2b12fd6702e68bac5daaf9928b
drkohli <drkohlipk@gmail.com>
2022-10-31 09:55:28 -0400

    Halloween python dailies

---
+++ python/dailies/31_oct_22/dfs.py
@@ -0,0 +1,35 @@
+from optparse import Option
+from typing import Optional, Set
+
+from helpers.graph_classes.graph_node import GraphNode
+from helpers.testing.graph_search_tester import GraphSearchTester
+
+
+def depth_first_search(
+        root: Optional[GraphNode],
+        identifier: int,
+        visited: Optional[Set[GraphNode]] = None,
+) -> Optional[GraphNode]:
+    if root is None or root.get_id() == identifier:
+        return root
+
+    if visited is None:
+        visited: Set[GraphNode] = set()
+
+    visited.add(root)
+
+    n = None
+
+    for child in root.get_children():
+        if child not in visited:
+            n = depth_first_search(
+                child, identifier, visited
+            )
+
+        if n is not None:
+            break
+
+    return n
+
+
+GraphSearchTester(depth_first_search)
---
+++ python/dailies/31_oct_22/merge_sort.py
@@ -0,0 +1,35 @@
+from typing import List
+from helpers.testing.list_sort_tester import ListSortTester
+
+
+def _merge(
+    lst_1: List[int], lst_2: List[int]
+) -> List[int]:
+    merged_lst: List[int] = []
+    i = 0
+    j = 0
+
+    while len(merged_lst) < (len(lst_1) + len(lst_2)):
+        if j >= len(lst_2) or (i < len(lst_1) and lst_1[i] <= lst_2[j]):
+            merged_lst.append(lst_1[i])
+            i += 1
+        else:
+            merged_lst.append(lst_2[j])
+            j += 1
+
+    return merged_lst
+
+
+def merge_sort(lst: List[int]) -> List[int]:
+    if len(lst) > 1:
+        mid = len(lst) // 2
+
+        lst = _merge(
+            merge_sort(lst[mid:]),
+            merge_sort(lst[:mid])
+        )
+
+    return lst
+
+
+ListSortTester(merge_sort)
